77298ea131ca0f3fb96db15137fcfd0965b6d5aa
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2022-10-16 09:02:35 +0300

    brain_games.py changed

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,9 +3,9 @@ import brain_games.cli
 
 
 def main():
-	print('Welcome to the Brain Games!')
-	brain_games.cli.welcome_user()
+    print('Welcome to the Brain Games!')
+    brain_games.cli.welcome_user()
 
 
 if __name__ == '__main__':
-	main()
+    main()
