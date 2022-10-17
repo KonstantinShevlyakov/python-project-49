f263372f89ae214ece95555b7ddc578be95682cd
Konstantin Shevlyakov <k.shevlyakov@Konstantins-MacBook-Air.local>
2022-10-17 16:25:01 +0300

    brain_even.py changed

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,29 +1,9 @@
-#!usr/bin/evn python3
-# import brain_games.scripts.brain_games
-from random import randint
-import brain_games.announcements
-from brain_games.announcements import get_result
-import prompt
-
-
-def is_even(number):
-    return number % 2 == 0 and 'yes' or 'no'
+#!usr/bin/env python3
+from brain_games.games.game_even import main as even
 
 
 def main():
-    name = brain_games.announcements.main()
-    count = 0
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    while count < brain_games.announcements.rounds_count():
-        random_int = randint(1, 100)
-        print(f'Question: {random_int}')
-        answer = prompt.string('Your answer: ')
-        correct_answer = is_even(random_int)
-        result = get_result(answer, correct_answer, name, count)
-        if result == 0:
-            break
-        else:
-            count += 1
+    even()
 
 
 if __name__ == '__main__':
